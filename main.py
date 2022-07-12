b71d0326a920a4a0b1cb4e1ebd8a4b0b256c148f
PavelG1307 <julu13@yandex.ru>
2022-07-13 00:25:10 +0300

    dev version

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ import json
 import sys
 
 key = 'af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir'
-cookie = 'u=2ospkoq5.q3xuox.7fs4jiv87n4; buyer_local_priority_v2=0; _ym_uid=1623759258873598042; sessid=5887d4513ea2d827c428b3d9b8dc93ad.1626951703; _ym_d=1641900929; __gads=ID=62e4b90be5a4d21a:T=1625657273:S=ALNI_MYDSgTp12uCkXfw-7HFLriUehI8TQ; adrcid=AdN-BkbrhKDUxXDsFGH8omQ; tmr_lvid=b8ddbd48e59db7c09ea3e12b1ba5e547; tmr_lvidTS=1653044851924; _gcl_au=1.1.1656198276.1653987768; buyer_location_id=637640; buyer_laas_location=637640; _ym_isad=2; _gid=GA1.2.1553517392.1657627701; f=5.9fd3735f16182a28b32428cf8e3c6b5047e1eada7172e06c47e1eada7172e06c47e1eada7172e06c47e1eada7172e06cb59320d6eb6303c1b59320d6eb6303c1b59320d6eb6303c147e1eada7172e06c8a38e2c5b3e08b898a38e2c5b3e08b890df103df0c26013a0df103df0c26013a2ebf3cb6fd35a0ac0df103df0c26013a8b1472fe2f9ba6b99364cc9ca0115366433be0669ea77fc074c4a16376f87ccd915ac1de0d034112ad09145d3e31a56946b8ae4e81acb9fae2415097439d4047e992ad2cc54b8aa8c772035eab81f5e1e992ad2cc54b8aa8d99271d186dc1cd03de19da9ed218fe23de19da9ed218fe2c772035eab81f5e1143114829cf33ca746b8ae4e81acb9fa38e6a683f47425a8352c31daf983fa077a7b6c33f74d335c76ff288cd99dba4604438f9f51f7fb331d49c1c93fcdfd7461c4c4543acfc7034638ec26cf48d72d17c7721dca45217ba1938a91a59ce5e68f97af514fc92d30e2415097439d404746b8ae4e81acb9fa786047a80c779d5146b8ae4e81acb9fa410b4b42af931ca18edb85158dee9a662da10fb74cac1eabb3ae333f3b35fe91de6c39666ae9b0d73339614c6f29137599c7f25537e9455a; ft="rPIYWUMAhsBO7vHyWe3pzwAspfLHaMngksgux4AC7MRzIUAOOy8RCs0IAwm5zWhAQF2hQmzCNJn3UGPR0/qofmPygDsr25JArSjABvQui4eSKqnEltcUYMSmqy22SuNR5WOY0lHjZAkw0gHKBhRnksYYvMkIEi8mvPpyVu3V44ODvqIwPkSJ/utNDMUcGz8G"; uxs_uid=54224b80-01dc-11ed-abeb-090cbed4b192; __ddg1_=Kr3srU2vCr43yNfdkN36; luri=moskva; st=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidko0TkJ5SlU2TElJTGRDY3VNTkdBZm5PRWwySzdtS01KZXBFMDh1VUFISWtQdTBhVzcyRHpGR3NKSHVJWTBqVktoSlQyVXBudTdSandaaktOc0YwV0tuUjZwMzZlNVpjR01mNjVsTTJHc3FHZmNHOVdJWHV0R2VPRjVrRXhwSDcxRk9UMzdCZUdJSDZUNEI3blVzNGVKSUszR3NVNTVkc1dsSE9vVjFudXdhdzBrN0t2VlRYckhlWHlSWDhJMGxuK3U5Ymk0MjBGbkNTMlBOY3FaVWpia1Q3Z0RWVW9tL1I4QW5DRWhXWktnV1ZjR1lQM2k4aGF2cVdvWUNnNEQ1M2hmeThlOEFJWTZIUlAxWU84RnpIaWhDWWswV2pjQ3M4Yk1ydXBIcHJIbk9IMkV1NTBsSW5XMkZBZmhWR0RxTEM0VC9taytIblhiRHBuVFdwNnJ4cjV3VjJ4cUVNM3lPbjJSRThodHRMYXhYYnN3WGdKWE4wNnZFTmRqd1RtYmRJbHlubXN6VE9WbjRNZWg5WVd4RVFDQWZqK3R3OHhUQnJMSVFPRUNtOE55MXZER3AzNWlET2orQ1UvcjdURHQ2VjhiaXViMFNaSEh2Wng4di9OTzdkc1pTdUNuMTIzKzk4bTVkeC9rN1VXTHVTblQ3elZKQit6Q2NXUmUxY3ppaTI0dWs2TGZWUEUzSFBKTUpIbzdJTE9QWVFUbXhyRjBLVldVdXJKc2NVMGo4NWpxVFFORXltZllzOCtoeUhyalpsRFpkdzJvOWJrSTVYSy9rbVVTQWVhc2JvVGY1K2hpZ0JsTDZkL1dhMVlFaFpQSDgxcHlTeTBXU2dWYmRIK21odHV1eXR4RDFTUzNHUFIxWWNlejQySkllWHhjbDVYdlZqRVRjWVE4aWlIS1dRQVkzeW5sSTBWZXNGYW40Yis3Y2NsOWMrTFZqTzVRNEhTeEZkVGcwNlZPQTlFaXIwYjdySm51RTRQN3lwOVBLTG9hOHpPREI4NFpaQmlRdjVBNTBvVGtCdHVyQnk0NTR1OGFHaW0yYm9Dckl2RkhyZmpoMEJ1RmNYbE4zNEhkT0FCVVl4SUZtc1g1OUg0dVJva2FodFl5VWtmc05GYUFqZnlZQnd6Q3BXSGd1R0JqVE1wYU1uWWtHNkRxRWRFcithaC9WREg1TzdhQjczSmxtd2pncWhQNmRudVluL0wwdzcrZ3BwTzRDQkhSczFaSXoxNU5qYmxzZlE5cTF6cmlZVFBwdm5EM1BCTUExWFY5czlmTmRFMTZUSEdTSzJUQ01Va05EcWh2Wm1tVzFpTzI1c0k2RXlMTWM0SmsrQkw1RzhZSWZVWDhTTDZBelVUMU95dmR4TyIsImlhdCI6MTY1NzYzMDg5MSwiZXhwIjoxNjU4ODQwNDkxfQ.tZrgR-ddiPR84i9At68Tabnp9kAzIa-gcYkIhsMlWYg; _ym_visorc=w; v=1657632306; redirectMav=1; sx=H4sIAAAAAAAC%2F5zSSZKjMBCF4buwZqFUaqzbaDbIILDM2MHdO7xwhb2tC3zxK%2FX%2BNURjBGuMEUYRi4qiF4RSo5VHGj02P%2F%2BatflpqCv7Rud0pOKO6bkfe0eyXHZJce66R9M2ofkBwaUErQm92oYRVIyBJFFHzrWgVlJJNBpExazTb1meNub5uZBY%2FHLrPLuf9SScx%2BkwroufsqSor7bhSnuUxgMlStEIXgOA5DTyGK00v839nnOkZ9%2F7OUQR7byYwIvEuz36s5%2B%2BmpViL7mMpsiAbku1pJyLYmVLxdU3OUTZWxjqBDYBS924Au%2BB66GyPajlKxbUK1YIIZyXImqhuWBCB2kDai85cU763zOMEJd%2BPhPxGrbx6LW%2F96fGdch6evBPmRKkcLWN8hoCtZSisFw6GnUMTEUFjhBg6N7yrVvXM7HFbwDMgA1kC7V7kPG8P4d7%2FjqDlOpqGz2w%2BbGOa5gqcTnnXFUptar0Jm1az20LPAygt64cI0mYhIbHgz9V2D5JxilebWPzMekZl7kqV3LOLmdSSmXsd2DLMrqbNIcY1zR2w3SrZx86DtIMa%2Bw%2BSUXxNTBXXFBPJfzrjypLr8iSS61%2FqUQQV9t4wYV10hkuhdPBkBCUCcIwKrxRQr3lO2oyb%2F6IO6uVHhn9WAPdp5MwPt%2FO7%2FcjudomOhHQMWMYtzoEBOKVi5xQzjil4P%2FSDAyv638AAAD%2F%2F4pO1YvDAwAA; _mlocation=637640; _mlocation_mode=laas; _ga=GA1.2.418341391.1625657273; tmr_detect=0%7C1657633935657; tmr_reqNum=203; _ga_9E363E7BES=GS1.1.1657627701.64.1.1657633957.19' \
-
+cookie = '_ga_9E363E7BES=GS1.1.1657659578.1.1.1657659740.60; v=1657659576; _buzz_fpc=JTdCJTIycGF0aCUyMiUzQSUyMiUyRiUyMiUyQyUyMmRvbWFpbiUyMiUzQSUyMi5tLmF2aXRvLnJ1JTIyJTJDJTIyZXhwaXJlcyUyMiUzQSUyMldlZCUyQyUyMDEyJTIwSnVsJTIwMjAyMyUyMDIxJTNBMDElM0EwNiUyMEdNVCUyMiUyQyUyMlNhbWVTaXRlJTIyJTNBJTIyTGF4JTIyJTJDJTIydmFsdWUlMjIlM0ElMjIlN0IlNUMlMjJ2YWx1ZSU1QyUyMiUzQSU1QyUyMmQ1ZjMzMDljNzgwZjQ3ZDUxMzU2ZDg3YjhiYjJkZGMxJTVDJTIyJTJDJTVDJTIyZnBqc0Zvcm1hdCU1QyUyMiUzQXRydWUlN0QlMjIlN0Q=; dfp_group=47; _mlocation=637640; _mlocation_mode=laas; st=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidko0TkJ5SlU2TElJTGRDY3VNTkdBZm5PRWwySzdtS01KZXBFMDh1VUFISlY4RUZWMTljb3lDSS82RnpVNGU4WU5NN0tKeS9OekhKTk9KaUNUcXN6Ny9HblJPMVlnY3Fxcml1RzZuZzBLZ1Q3Z2lGNUZjRFQ1aFlraUhPM004b1hCWVRpemIvSzcySjIrTGRLREY1QWJjdHVVYjNHNWdSNENhemZRSUI3aWI5Ylo5S0R2VngxaDc3b3puS3NRMXZjbklFV2JjQng1ZlF6TzJhNngyb2g3cTF3emRrYTlVNm9sRGh2bjJDWkcvTjJ6VHdzV0ZtVGtPSUpFTTJwa1ZpVkZ4TzBMUlFPMVdPN045a1ZPdTgvNUFDU29QVXdDOC80N1VDMlNoTVJUcEl4T0ZQcVpPNnJ2amRyU0h3d2RjMTZZN0JxSGEwQm10OG9FS2c4NHQwL1ljelRBMWtteUplcjNXbTdJNTNBSTdGMzFzWWJqNENjSy9JK3BIbUFJbFFCYzBqdkw1QXRLM0F2UnJpRzRGa3lWUDNMSFRobTVZQ3ZUVGVCc09hMk5CcExEZStqQS9DaHlZOGlHS3RVZjhuOUlxdlRqZFJrQ1ZRNE5GSGsxYkc3Z3FYRXhJTXB1T0hLMXJ0NlJQUCtpZnBWN0RjZllHNEJRWGpQNmEvSHpaR3oyVkwwdUZValovM2w4RURGeHpMZlM3UnpnRU1Nb3RIM0Y3Z3pkalRFcGorNXBqd3FEQWF1S016T1U2RUhmb1NDWGd5VjFocXJZRmd1bDJ3WEhVV1RwaEJsSHRwNkFITWJhZXhZTW5aWWtjYjFpbHA3b0NKbGUwaFZtMWUxUzFWKzIzWERlRU1VZHZaUm5SMTM5TmtrZzU3S1YwdEYwdVVxWGVDNGpaZHhXN1FnWVdxUmhEK0QyV1hSUDJNSUw1eXpxcllmSzVUNXVUUHNBaGFTM1pXamlpd2REdmVuejloMzdQWmI2Vkw1dElSeWhFN1JDMkppcXlJczZLMStsVUVwRlNDVFV2N3IzdHMwYk9oMUFWUndaTHYxNnVMVmMzMVpOL2pKd0Y2M29SV2xCOWZwRjZ5Y2ZHNDhMbCttSmdjTUJySEtWRXFnWlowTExIaFgzeitBek9na1Z5Y1pCQW0zM1lxRFUvSm8wOHRCN2lLSVl1cTE1NVVoOUNuenZJeTZEZENxTmxiYmE1TXZGT2g3WlhOWnp3PT0iLCJpYXQiOjE2NTc2NTk2MDEsImV4cCI6MTY1ODg2OTIwMX0.JnwxxWb0pR_hMDBCWcRUJp9V9EY8fXLBtUX4KsYhXJU; tmr_reqNum=7; tmr_detect=0%7C1657659644403; _ga=GA1.1.114331317.1657659578; _gid=GA1.2.1489576562.1657659579; tmr_lvid=eeef370a7b3370f0eb8e36fa0b487125; tmr_lvidTS=1657659578839; f=5.e1ae7f0eb55e91e9c738a39f956d9a5c16d443061c57ac4216d443061c57ac4216d443061c57ac4216d443061c57ac42cec4d980e289734fcec4d980e289734fcec4d980e289734f96a296658223dafb96a296658223dafb381ebd593ffbb4c30df103df0c26013a8b1472fe2f9ba6b90df103df0c26013a268a7bf63aa148d2ea5c223d6b439c9d5f5fef956c8c12f948717a87126911fa1cbff88f85149ecf4a5611862bd91ca7e2415097439d4047143114829cf33ca787829363e2d856a2d50b96489ab264edc772035eab81f5e187829363e2d856a2f88859c11ff008953de19da9ed218fe23de19da9ed218fe2c772035eab81f5e13de19da9ed218fe2143114829cf33ca7bed76bde8afb15d28e3a80a29e104a6c2c61f4550df136d8b52ccfba5f649ac597bc64b9d3449ed41da9f488c469f075bcaaee87a280b624d6eebeac28b9a9f02d57da0d3d3274b10616349da3da33c3f009e5192741225146b8ae4e81acb9fa46b8ae4e81acb9fa02c68186b443a7ac7c629922cb33e217f1f94a407205ae132da10fb74cac1eab2da10fb74cac1eab15fe31d52e5b1f9a711bc502486cd628; ft="VfLqAIggFQ9o/I6cm9jBoogxlhfMy+sJAk0HaglTzn8iH05//tOB46I2U3tfXTf398CNJrVT5JwvUcvgQU95TaT3PZMUlJ9AfH7tim9I6v93wEW4yC72FwnqtL7hyf+EZS8TBMlyi+QRUnsvAfab72iREtkZNt18pgw/AcS3pyjX34a26Kn3syV8BP6UIHHL"; adrcid=A6-8WejCF6isSNpbB8jvpSA; adrdel=1; _gcl_au=1.1.601596732.1657659578; u=2te6ck1c.1cwcd17.7b32ehurnuk0'
 search = 'склад'
 categoryId = 42
 locationId = 637640
 sort = 'priceDesc'
 withImagesOnly = 'false'
 limit_page = 50
-
 s = requests.Session()
-
+# https://m.avito.ru/api/1/rmp/search?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&params[536]=5546&params[110799]=472645&query=%D1%81%D0%BA%D0%BB%D0%B0%D0%B4&categoryId=42&locationId=637640&searchRadius=0
 headers = { 'authority': 'm.avito.ru',
             'pragma': 'no-cache',
             'cache-control': 'no-cache',
@@ -26,16 +24,16 @@ headers = { 'authority': 'm.avito.ru',
             'sec-fetch-user': '?1',
             'sec-fetch-dest': 'document',
             'accept-language': 'ru-RU,ru;q=0.9',}
-if cookie:                                      # Добавим куки, если есть внешние куки
+if cookie:                                    
     headers['cookie'] = cookie
-s.headers.update(headers)                       # Сохраняем заголовки в сессию
-s.get('https://m.avito.ru/')                    # Делаем запрос на мобильную версию.
-url_api_9 = 'https://m.avito.ru/api/11/items'    # Урл первого API, позволяет получить id и url объявлений по заданным фильтрам
-                                                # Тут уже видно цену и название объявлений
+s.headers.update(headers)                      
+print(s.get('https://m.avito.ru/').text)                   
+url_api_9 = 'https://m.avito.ru/api/1/rmp/'
+
 params = {
     'categoryId': categoryId,
     'params[536]': 5546,
-    'params[554]': 5727,
+    'params[110799]': 472645,
     'locationId': locationId,
     'sort': sort,
     'withImagesOnly': withImagesOnly,
@@ -43,9 +41,8 @@ params = {
     'display': 'list',
     'limit': limit_page,
     'query': search,
+    'key': key,
 }
-
-params['key'] =  key
 
 
 def writeInLog(message, location):
@@ -69,6 +66,7 @@ def getIDS(filename = 'ids.ini'):
         except json.decoder.JSONDecodeError:
             cicle_stop = False
             break
+        print(res)
 
         if res['status'] != 'ok':
                 print(res)
@@ -94,7 +92,7 @@ def getIDS(filename = 'ids.ini'):
 
 
 def getInfo(id):
-    url_info = f'https://m.avito.ru/api/18/items/{id}'
+    url_info = f'https://m.avito.ru/api/1/rmp/show/{id}'
     params = {
         'key': key
     }
@@ -168,32 +166,34 @@ def SaveInfo(info, file_name = 'data.csv'):
 
 
 def main():
-    
-    title_csv = ['Название','Адрес','URL',
-             'Цена','Координаты lat','Координаты lng',
-             'Тип здания', 'Класс здания', 'Общая площадь', 
-             'Этаж', 'Отделка', 'Залог', 
-             'Комиссия, %', 'Категория', 'Парковка', 
-             'Тип парковки', 'Отдельный вход','Общая площадь',
-             'Аренда части площади','Высота потолков, м','Включено в стоимость',
-             'Отопление','Отделка','Вход',
-             'Отдельный вход', 'Описание','Изображения', 
-             'Дата опубликования', 'Тип продавца','Название компании',
-             'Имя продавца','URL продавца']
+    res = s.get('https://m.avito.ru/api/1/rmp/show/2338321304?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir')
+    print(res)
+    # title_csv = ['Название','Адрес','URL',
+    #          'Цена','Координаты lat','Координаты lng',
+    #          'Тип здания', 'Класс здания', 'Общая площадь', 
+    #          'Этаж', 'Отделка', 'Залог', 
+    #          'Комиссия, %', 'Категория', 'Парковка', 
+    #          'Тип парковки', 'Отдельный вход','Общая площадь',
+    #          'Аренда части площади','Высота потолков, м','Включено в стоимость',
+    #          'Отопление','Отделка','Вход',
+    #          'Отдельный вход', 'Описание','Изображения', 
+    #          'Дата опубликования', 'Тип продавца','Название компании',
+    #          'Имя продавца','URL продавца']
     
     # file_ids = getIDS('ids.ino')
-    file_ids = 'ids.ini'
-    f = open(file_ids, mode = 'r')
-    ids = f.readlines()
-    SaveInfo(title_csv)
-    for i in range(len(ids)):
-        info = getInfo(ids[i].strip())
-        parse_info = ParseInfo(info, title_csv)
-        if parse_info:
-            SaveInfo(parse_info)
-        else:
-            print(f'Ошибка на {i} объявлении, id: {ids[i]}')
-            writeInLog(f'Ошибка на {i} объявлении, id: {ids[i]}', 'main')
+    # file_ids = 'ids.ini'
+    # f = open(file_ids, mode = 'r')
+    # ids = f.readlines()
+    # SaveInfo(title_csv)
+    # for i in range(len(ids)):
+    #     info = getInfo(ids[i].strip())
+    #     parse_info = ParseInfo(info, title_csv)
+    #     if parse_info:
+    #         SaveInfo(parse_info)
+    #     else:
+    #         print(f'Ошибка на {i} объявлении, id: {ids[i]}')
+    #         writeInLog(f'Ошибка на {i} объявлении, id: {ids[i]}', 'main')
+    #         input('Нажмите Enter чтобы продолжить...')
     # f = open('test.json', mode = 'r')
     # info = json.load(f)
     # ParseInfo(info)
