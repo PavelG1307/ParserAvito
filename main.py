0c413681db9c79b131ed82a3ee6e0dedc55e5734
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-12 18:27:47 +0300

    parse

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import requests, json, sys
+import requests
+import csv
+import json
+import sys
 
 key = 'af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir'
-# cookie = '__cfduid=da6b6b5b9f01fd022f219ed53ac3935791610912291; sessid=ef757cc130c5cd228be88e869369c654.1610912291; _ga=GA1.2.559434019.1610912292; _gid=GA1.2.381990959.1610912292; _fbp=fb.1.1610912292358.1831979940; u=2oiycodt.1oaavs8.dyu0a4x7fxw0; v=1610912321; buyer_laas_location=641780; buyer_location_id=641780; luri=novosibirsk; buyer_selected_search_radius4=0_general; buyer_local_priority_v2=0; sx=H4sIAAAAAAACAxXLQQqAIBAF0Lv8dYvRLEdvU0MIBU0iKCHePXr71zGfefd1W5RLYick2kSakiB2VETclpf85n19RJMSp4vJOSlM%2F2BMOBDNaigE9taM8QH0oydNVAAAAA%3D%3D; dfp_group=100; _ym_uid=1610912323905107257; _ym_d=1610912323; _ym_visorc_34241905=b; _ym_isad=2; _ym_visorc_419506=w; _ym_visorc_188382=w; __gads=ID=2cff056a4e50a953-22d0341a94b900a6:T=1610912323:S=ALNI_MZMbOe0285QjW7EVvsYtSa-RA_Vpg; f=5.8696cbce96d2947c36b4dd61b04726f1a816010d61a371dda816010d61a371dda816010d61a371dda816010d61a371ddbb0992c943830ce0bb0992c943830ce0bb0992c943830ce0a816010d61a371dd2668c76b1faaa358c08fe24d747f54dc0df103df0c26013a0df103df0c26013a2ebf3cb6fd35a0ac0df103df0c26013a8b1472fe2f9ba6b978e38434be2a23fac7b9c4258fe3658d831064c92d93c3903815369ae2d1a81d04dbcad294c152cb0df103df0c26013a20f3d16ad0b1c5462da10fb74cac1eab2da10fb74cac1eab3c02ea8f64acc0bdf0c77052689da50d2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab91e52da22a560f5503c77801b122405c48ab0bfc8423929a6d7a5083cc1669877def5708993e2ca678f1dc04f891d61e35b0929bad7c1ea5dec762b46b6afe81f200c638bc3d18ce60768b50dd5e12c30e37135e8f7c6b64dc9f90003c0354a346b8ae4e81acb9fa46b8ae4e81acb9fa02c68186b443a7acf8b817f3dc0c3f21c1eac53cc61955882da10fb74cac1eab2da10fb74cac1eab5e5aa47e7d07c0f95e1e792141febc9cb841da6c7dc79d0b' \
+cookie = 'u=2ospkoq5.q3xuox.7fs4jiv87n4; buyer_local_priority_v2=0; _ym_uid=1623759258873598042; sessid=5887d4513ea2d827c428b3d9b8dc93ad.1626951703; _ym_d=1641900929; __gads=ID=62e4b90be5a4d21a:T=1625657273:S=ALNI_MYDSgTp12uCkXfw-7HFLriUehI8TQ; adrcid=AdN-BkbrhKDUxXDsFGH8omQ; tmr_lvid=b8ddbd48e59db7c09ea3e12b1ba5e547; tmr_lvidTS=1653044851924; _gcl_au=1.1.1656198276.1653987768; buyer_location_id=637640; buyer_laas_location=637640; _ym_isad=2; _gid=GA1.2.1553517392.1657627701; f=5.9fd3735f16182a28b32428cf8e3c6b5047e1eada7172e06c47e1eada7172e06c47e1eada7172e06c47e1eada7172e06cb59320d6eb6303c1b59320d6eb6303c1b59320d6eb6303c147e1eada7172e06c8a38e2c5b3e08b898a38e2c5b3e08b890df103df0c26013a0df103df0c26013a2ebf3cb6fd35a0ac0df103df0c26013a8b1472fe2f9ba6b99364cc9ca0115366433be0669ea77fc074c4a16376f87ccd915ac1de0d034112ad09145d3e31a56946b8ae4e81acb9fae2415097439d4047e992ad2cc54b8aa8c772035eab81f5e1e992ad2cc54b8aa8d99271d186dc1cd03de19da9ed218fe23de19da9ed218fe2c772035eab81f5e1143114829cf33ca746b8ae4e81acb9fa38e6a683f47425a8352c31daf983fa077a7b6c33f74d335c76ff288cd99dba4604438f9f51f7fb331d49c1c93fcdfd7461c4c4543acfc7034638ec26cf48d72d17c7721dca45217ba1938a91a59ce5e68f97af514fc92d30e2415097439d404746b8ae4e81acb9fa786047a80c779d5146b8ae4e81acb9fa410b4b42af931ca18edb85158dee9a662da10fb74cac1eabb3ae333f3b35fe91de6c39666ae9b0d73339614c6f29137599c7f25537e9455a; ft="rPIYWUMAhsBO7vHyWe3pzwAspfLHaMngksgux4AC7MRzIUAOOy8RCs0IAwm5zWhAQF2hQmzCNJn3UGPR0/qofmPygDsr25JArSjABvQui4eSKqnEltcUYMSmqy22SuNR5WOY0lHjZAkw0gHKBhRnksYYvMkIEi8mvPpyVu3V44ODvqIwPkSJ/utNDMUcGz8G"; uxs_uid=54224b80-01dc-11ed-abeb-090cbed4b192; __ddg1_=Kr3srU2vCr43yNfdkN36; luri=moskva; st=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidko0TkJ5SlU2TElJTGRDY3VNTkdBZm5PRWwySzdtS01KZXBFMDh1VUFISWtQdTBhVzcyRHpGR3NKSHVJWTBqVktoSlQyVXBudTdSandaaktOc0YwV0tuUjZwMzZlNVpjR01mNjVsTTJHc3FHZmNHOVdJWHV0R2VPRjVrRXhwSDcxRk9UMzdCZUdJSDZUNEI3blVzNGVKSUszR3NVNTVkc1dsSE9vVjFudXdhdzBrN0t2VlRYckhlWHlSWDhJMGxuK3U5Ymk0MjBGbkNTMlBOY3FaVWpia1Q3Z0RWVW9tL1I4QW5DRWhXWktnV1ZjR1lQM2k4aGF2cVdvWUNnNEQ1M2hmeThlOEFJWTZIUlAxWU84RnpIaWhDWWswV2pjQ3M4Yk1ydXBIcHJIbk9IMkV1NTBsSW5XMkZBZmhWR0RxTEM0VC9taytIblhiRHBuVFdwNnJ4cjV3VjJ4cUVNM3lPbjJSRThodHRMYXhYYnN3WGdKWE4wNnZFTmRqd1RtYmRJbHlubXN6VE9WbjRNZWg5WVd4RVFDQWZqK3R3OHhUQnJMSVFPRUNtOE55MXZER3AzNWlET2orQ1UvcjdURHQ2VjhiaXViMFNaSEh2Wng4di9OTzdkc1pTdUNuMTIzKzk4bTVkeC9rN1VXTHVTblQ3elZKQit6Q2NXUmUxY3ppaTI0dWs2TGZWUEUzSFBKTUpIbzdJTE9QWVFUbXhyRjBLVldVdXJKc2NVMGo4NWpxVFFORXltZllzOCtoeUhyalpsRFpkdzJvOWJrSTVYSy9rbVVTQWVhc2JvVGY1K2hpZ0JsTDZkL1dhMVlFaFpQSDgxcHlTeTBXU2dWYmRIK21odHV1eXR4RDFTUzNHUFIxWWNlejQySkllWHhjbDVYdlZqRVRjWVE4aWlIS1dRQVkzeW5sSTBWZXNGYW40Yis3Y2NsOWMrTFZqTzVRNEhTeEZkVGcwNlZPQTlFaXIwYjdySm51RTRQN3lwOVBLTG9hOHpPREI4NFpaQmlRdjVBNTBvVGtCdHVyQnk0NTR1OGFHaW0yYm9Dckl2RkhyZmpoMEJ1RmNYbE4zNEhkT0FCVVl4SUZtc1g1OUg0dVJva2FodFl5VWtmc05GYUFqZnlZQnd6Q3BXSGd1R0JqVE1wYU1uWWtHNkRxRWRFcithaC9WREg1TzdhQjczSmxtd2pncWhQNmRudVluL0wwdzcrZ3BwTzRDQkhSczFaSXoxNU5qYmxzZlE5cTF6cmlZVFBwdm5EM1BCTUExWFY5czlmTmRFMTZUSEdTSzJUQ01Va05EcWh2Wm1tVzFpTzI1c0k2RXlMTWM0SmsrQkw1RzhZSWZVWDhTTDZBelVUMU95dmR4TyIsImlhdCI6MTY1NzYzMDg5MSwiZXhwIjoxNjU4ODQwNDkxfQ.tZrgR-ddiPR84i9At68Tabnp9kAzIa-gcYkIhsMlWYg; _ym_visorc=w; v=1657632306; redirectMav=1; sx=H4sIAAAAAAAC%2F5zSSZKjMBCF4buwZqFUaqzbaDbIILDM2MHdO7xwhb2tC3zxK%2FX%2BNURjBGuMEUYRi4qiF4RSo5VHGj02P%2F%2BatflpqCv7Rud0pOKO6bkfe0eyXHZJce66R9M2ofkBwaUErQm92oYRVIyBJFFHzrWgVlJJNBpExazTb1meNub5uZBY%2FHLrPLuf9SScx%2BkwroufsqSor7bhSnuUxgMlStEIXgOA5DTyGK00v839nnOkZ9%2F7OUQR7byYwIvEuz36s5%2B%2BmpViL7mMpsiAbku1pJyLYmVLxdU3OUTZWxjqBDYBS924Au%2BB66GyPajlKxbUK1YIIZyXImqhuWBCB2kDai85cU763zOMEJd%2BPhPxGrbx6LW%2F96fGdch6evBPmRKkcLWN8hoCtZSisFw6GnUMTEUFjhBg6N7yrVvXM7HFbwDMgA1kC7V7kPG8P4d7%2FjqDlOpqGz2w%2BbGOa5gqcTnnXFUptar0Jm1az20LPAygt64cI0mYhIbHgz9V2D5JxilebWPzMekZl7kqV3LOLmdSSmXsd2DLMrqbNIcY1zR2w3SrZx86DtIMa%2Bw%2BSUXxNTBXXFBPJfzrjypLr8iSS61%2FqUQQV9t4wYV10hkuhdPBkBCUCcIwKrxRQr3lO2oyb%2F6IO6uVHhn9WAPdp5MwPt%2FO7%2FcjudomOhHQMWMYtzoEBOKVi5xQzjil4P%2FSDAyv638AAAD%2F%2F4pO1YvDAwAA; _mlocation=637640; _mlocation_mode=laas; _ga=GA1.2.418341391.1625657273; tmr_detect=0%7C1657633935657; tmr_reqNum=203; _ga_9E363E7BES=GS1.1.1657627701.64.1.1657633957.19' \
 
 search = 'склад'
 categoryId = 42
@@ -23,8 +26,8 @@ headers = { 'authority': 'm.avito.ru',
             'sec-fetch-user': '?1',
             'sec-fetch-dest': 'document',
             'accept-language': 'ru-RU,ru;q=0.9',}
-# if cookie:                                      # Добавим куки, если есть внешние куки
-#     headers['cookie'] = cookie
+if cookie:                                      # Добавим куки, если есть внешние куки
+    headers['cookie'] = cookie
 s.headers.update(headers)                       # Сохраняем заголовки в сессию
 s.get('https://m.avito.ru/')                    # Делаем запрос на мобильную версию.
 url_api_9 = 'https://m.avito.ru/api/11/items'    # Урл первого API, позволяет получить id и url объявлений по заданным фильтрам
@@ -57,11 +60,13 @@ def getIDS(f):
         res = s.get(url_api_9, params=params)
         try:
             res = res.json()
+
         except json.decoder.JSONDecodeError:
             cicle_stop = False
             break
+
         if res['status'] != 'ok':
-                # print(res['result'])
+                print(res)
                 sys.exit(1)
         if res['status'] == 'ok':
             items_page = int(len(res['result']['items']))
@@ -83,27 +88,49 @@ def getIDS(f):
 
 
 def getInfo(id):
+    info = []
     url_info = f'https://m.avito.ru/api/18/items/{id}'
     params = {
         'key': key
     }
-    info = s.get(url_info, params=params).json()
-    print(info)
-    if not 'error' in info:
+    info_js = s.get(url_info, params=params).json()
+    if not 'error' in info_js:
         f = open('test.json', mode = 'w')
-        json.dump(info, f)
+        json.dump(info_js, f)
+    else:
+        print(info_js)
+        
+
+# additionalSeller.parameters
+
+def ParseInfo(info):
+    title = info['title']
+    adress = info['address']
+    lat = info['coords']['lat']
+    lng = info['coords']['lng']
+    description = info['description']
+    print()
+
+def SaveInfo(info):
+    csvFile = open('example2.csv', 'a')
+    with csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(info)
 
 
 def main():
     # f = open('ids.ino', mode = 'a')
     # ids = getIDS(f)
     # print(f'Всего объявлений: {len(ids)}')
-    f = open('ids.ino', mode = 'r')
-    ids = f.readlines()
-    for i in range(len(ids)):
-        ids[i] = ids[i].strip()
-    getInfo(ids[0])
-    print(ids)
+    # f = open('ids.ino', mode = 'r')
+    # ids = f.readlines()
+    # for i in range(len(ids)):
+    #     ids[i] = ids[i].strip()
+    # info = getInfo(ids[0])
+    # SaveInfo([[]])
+    f = open('test.json', mode = 'r')
+    info = json.load(f)
+    ParseInfo(info)
 
 if __name__ == '__main__':
     main()
@@ -132,22 +159,4 @@ if __name__ == '__main__':
 #         # print(more_data_2['description']) # Скрыл, т.к. много букв
 #         print('=======================================================\n')
 
-# params = {'key': key}
-# ad_id = '2448175824'
-# url_more_data_2 = 'https://m.avito.ru/api/15/items/' + ad_id
-# more_data_2 = s.get(url_more_data_2, params=params).json()
-# print(more_data_2)
-# if not 'error' in more_data_2:
-#     # print(more_data_2)            # В more_data_2 есть всё, что надо, я вывел на принт наиболее интересные для наглядности:
-#     print(more_data_2['title'])
-#     print(more_data_2['price'])
-#     print(more_data_2['address'])
-#     url_get_phone = 'https://m.avito.ru/api/1/items/' + ad_id + '/phone'    # URL для получения телефона
-#     phone = s.get(url_get_phone, params=params).json()                      # Сам запрос
-#     if phone['status'] == 'ok': phone_number = requests.utils.unquote(phone['result']['action']['uri'].split('number=')[1]) # Прверка на наличие телефона, такой странный синтсксис, чтоб уместиться в 100 сторочек кода)))
-#     else: phone_number = phone['result']['message']
-#     print(phone_number)
-#     print(more_data_2['seller'])
-#     # print(more_data_2['description']) # Скрыл, т.к. много букв
-#     print('=======================================================\n')
 
