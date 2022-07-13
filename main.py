adb9b244d592ff27d81e5dd00e1890b1345530ac
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-13 13:09:23 +0300

    Work version

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import requests
 import csv
 import json
 import sys
+import time
 
 
 key = 'af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir'
-cookie = 'u=2fkcz6l8.1kgj55.g3xn3vc0t3; _ym_uid=1560267406178859309; buyer_selected_search_radius4=0_general; buyer_selected_search_radius3=0_services; buyer_selected_search_radius2=0_job; buyer_selected_search_radius0=200; buyer_local_priority_v2=0; buyer_index_tooltip=1; auth=1; _gcl_au=1.1.733197779.1652816764; luri=moskva; _ym_d=1657654472; _ym_isad=1; tmr_lvid=5c9c483ac660d500846a4d65251b749d; tmr_lvidTS=1657654489421; _gid=GA1.2.2632564.1657654490; f=5.cc913c231fb04ced4b5abdd419952845a68643d4d8df96e9a68643d4d8df96e9a68643d4d8df96e9a68643d4d8df96e94f9572e6986d0c624f9572e6986d0c624f9572e6986d0c62ba029cd346349f36c1e8912fd5a48d02c1e8912fd5a48d0246b8ae4e81acb9fa143114829cf33ca746b8ae4e81acb9fa46b8ae4e81acb9fae992ad2cc54b8aa858f6718e375efe9248bdd0f4e425aba7085d5b6a45ae867378ba5f931b08c66a72a0378f24244a7a2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eab2da10fb74cac1eabf8e5e9b21a40e18dde87ad3b397f946b4c41e97fe93686adf74ad50eaa2b3372f415355acea9907602c730c0109b9fbb551b50768d5f707e6f9088c33a557ef70e28148569569b7903532f306346f575b9e8022cece544b32ebf3cb6fd35a0ac0df103df0c26013a28a353c4323c7a3a140a384acbddd74801d2e1440f9b5e4f3de19da9ed218fe23de19da9ed218fe2ddb881eef125a8703b2a42e40573ac3cd81d609ad4115c32bcdb5a5dea4d64cf; ft="4GDrFjTQl5E6tNlykiW1h6a0Clle3t6gkc53Ehj8v2y1ET6ospzoFOy7dn8fB7Q5ynjtpHR3ixYRr9hSVDGMQ0VWF/8Aj1WC46DJue9Xzl/lDZwauU413MCpVeR1MlqodYHJIwCzSLnD12o9lSmk00AXQQWiaYq6ALPFaLyt0iZIBbLKdHLmuFMvXt8GdaDX"; buyer_location_id=637640; st=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidko0TkJ5SlU2TElJTGRDY3VNTkdBZm5PRWwySzdtS01KZXBFMDh1VUFISkRoNkVQZGMzaUhBVjc3QlpVWDhYMG4xS0NKYklyeGVJZkVmSW9SL1FyRDI2dHVKY0dRQzNHODh2WVY5QjNGSWoyOURTOGhvZ2xaM1J5SUNlM3lpOXpuYzZXK0xGcGt3S1o4WmtabTdLT2dJSUFOajFsMzZhQjIyQXRNd1ZPeXZDR1RZRWVsYmtpT1k5TnJPdnE2enpTNzlDakZqTEM2b3JDTE1wNG4xUnl4eDdEQmZEWEpZTDJMZyt0WVZacm1EWjlZeFlxL3BIUEFjSDlBQjhoQmpVUmNKYzF0Qlc4TXhSUTNqUFN1cDBCYmpORUs3YmN1WHlQU0dyenZNdFgvSUk5bHpkN1RZOW9hb2UrVXVGQmhrMlg2ZVdwRUFGK3ZVUjZQYkxJME5zdmxoV3Q3c0p2OUp5eXNPdUZHNE5HVXYvZ1VkRDN2bjh2ZE9aeXNFeHFMNnB2cXVBd28wQXc1dm95cHZQNFcvYXVualNnd0pvWk5GVGZTWHRGY3BUVURXODhDaG9rNVh1U1NBWWU1QnMzWEVDMkYxdUs2K3ZXalRNQnVMTjNCMnFxSXA1N21LLzNYV1ZQcmlVeVBNTS8wN0ZaSXgwUHFwM3Q4bHBFQlh0dGNxdnNPNGhuSGhQRmJVVC94cVZ1OFhXQXYzTHpBZ1hSc3ozL0hKdXFYdGk0OGhzUEVpbkRrMlF0ZDJHNmx2QjJYNzlYRk1WVlc0MmtCWmgvaERQL0JqSjV4aTZTeVdSTEhLS1c5Q2dOcjJQWXpFVE00eW9CbUV6c2VEREtGT3lSQ2NxR25waCtyTGJsa2w0SHlOM2hQMTZWTENxb1g4NXZFUlJ1Tm9QbWYrTHlIcVB2TG9NODEvVVBHdGduU1puakJhWHE2M3FsU0pqcVJ0SjFOc0kwMXVIRzlOb0J4eSt6M3pudXN3MzVacWVFd1BiL2hiYmV5TldlTFlOZ1BxZ0FVdXl3Z2tMSWlMU3pJamVTZEZ4U05weWRBVDArRllWZFU5eWR5Zy9KTjhxaWFSVEtNSjhxY09ZSWhPS2hXdzdzQ3RHYkpZVTVoVHNoZVNidUticElCUVhYb0hDZzVFbGUzRmJLMVBDUUR3YUl5YkdHQmRYdEVWOVFXZHNZa3F3NjZoU0l5ek9iRGs2NVQzd29GelpsVzBHQTUzMWRxd21maW1qSWY2Smdxalg0M1hvMk43c1JPeTMycUxWdEY3QzNGajVmMUVhWkF3WHl6d3VOeUI5Y1I2bkM3aDdsajRMbTB4NU82Y0pqcnNCTFd3WVRFLzAzbjlnT0JvWk1yM3d3WlYyQiIsImlhdCI6MTY1NzY1NzgwNCwiZXhwIjoxNjU4ODY3NDA0fQ.u0ImCa7ThsN2Rdsil-cWSumjBAxXfzbYo1UpCEV03g4; _mlocation=637640; _mlocation_mode=laas; uxs_uid=a2c18040-0221-11ed-87ea-93fc87eb159f; v=1657661110; buyer_laas_location=637640; dfp_group=33; isLegalPerson=0; _ym_visorc=b; redirectMav=1; sx=H4sIAAAAAAAC/5zRSc7jIBBA4buw9oKhKIrcBgqM5wyOjJ1fvnsri0idbS7w6UnvTxhnSUuQllqrIgROrtVGkyHOySkUlz+xiYt4+YeNtb7Gg2/1GikvPN+GyfAzur3eRSOyuCi0zoGSIM9GICJycth69BYBfXYxG5+clcwu+Y/st3bhaeoWBzAOz9DnfOxHLZS2Tkr8X3Zo4WyEIxUxS8qOrMnJ2pZRplYnaayNmD7yQPMa58c9TsNTPVaAZ9+PPQCyvnPcv5s1no3w3ktvICFrDg4So1ZJK6OVU57IfmRKV1XrrGm7HS+oI7BSc6dyOdpoqv6WzbuZ22UfjqzjNNZVch3XIq9rgfVDBjncDjYHpWq3BTua9gHHLhTTdY/wRUrv37EJLUZ2HKxD9jnInClkDKAxBUL6SdbqbES2s6ZlHzNcoRQGWQoUgMo/kdadjejHMbbL3G87FyokQTKvhdefXllznv8CAAD//1V24H67AgAA; _ga=GA1.2.1264586811.1652816766; tmr_detect=0|1657661274813; tmr_reqNum=40; _ga_9E363E7BES=GS1.1.1657657753.4.1.1657661289.5'
+cookie = 'u=2ospkoq5.q3xuox.7fs4jiv87n4; buyer_local_priority_v2=0; _ym_uid=1623759258873598042; sessid=5887d4513ea2d827c428b3d9b8dc93ad.1626951703; _ym_d=1641900929; __gads=ID=62e4b90be5a4d21a:T=1625657273:S=ALNI_MYDSgTp12uCkXfw-7HFLriUehI8TQ; adrcid=AdN-BkbrhKDUxXDsFGH8omQ; tmr_lvid=b8ddbd48e59db7c09ea3e12b1ba5e547; tmr_lvidTS=1653044851924; _gcl_au=1.1.1656198276.1653987768; buyer_location_id=637640; buyer_laas_location=637640; _ym_isad=2; _gid=GA1.2.1553517392.1657627701; f=5.9fd3735f16182a28b32428cf8e3c6b5047e1eada7172e06c47e1eada7172e06c47e1eada7172e06c47e1eada7172e06cb59320d6eb6303c1b59320d6eb6303c1b59320d6eb6303c147e1eada7172e06c8a38e2c5b3e08b898a38e2c5b3e08b890df103df0c26013a0df103df0c26013a2ebf3cb6fd35a0ac0df103df0c26013a8b1472fe2f9ba6b99364cc9ca0115366433be0669ea77fc074c4a16376f87ccd915ac1de0d034112ad09145d3e31a56946b8ae4e81acb9fae2415097439d4047e992ad2cc54b8aa8c772035eab81f5e1e992ad2cc54b8aa8d99271d186dc1cd03de19da9ed218fe23de19da9ed218fe2c772035eab81f5e1143114829cf33ca746b8ae4e81acb9fa38e6a683f47425a8352c31daf983fa077a7b6c33f74d335c76ff288cd99dba4604438f9f51f7fb331d49c1c93fcdfd7461c4c4543acfc7034638ec26cf48d72d17c7721dca45217ba1938a91a59ce5e68f97af514fc92d30e2415097439d404746b8ae4e81acb9fa786047a80c779d5146b8ae4e81acb9fa410b4b42af931ca18edb85158dee9a662da10fb74cac1eabb3ae333f3b35fe91de6c39666ae9b0d73339614c6f29137599c7f25537e9455a; ft="rPIYWUMAhsBO7vHyWe3pzwAspfLHaMngksgux4AC7MRzIUAOOy8RCs0IAwm5zWhAQF2hQmzCNJn3UGPR0/qofmPygDsr25JArSjABvQui4eSKqnEltcUYMSmqy22SuNR5WOY0lHjZAkw0gHKBhRnksYYvMkIEi8mvPpyVu3V44ODvqIwPkSJ/utNDMUcGz8G"; uxs_uid=54224b80-01dc-11ed-abeb-090cbed4b192; __ddg1_=Kr3srU2vCr43yNfdkN36; luri=moskva; st=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidko0TkJ5SlU2TElJTGRDY3VNTkdBZm5PRWwySzdtS01KZXBFMDh1VUFISWtQdTBhVzcyRHpGR3NKSHVJWTBqVktoSlQyVXBudTdSandaaktOc0YwV0tuUjZwMzZlNVpjR01mNjVsTTJHc3FHZmNHOVdJWHV0R2VPRjVrRXhwSDcxRk9UMzdCZUdJSDZUNEI3blVzNGVKSUszR3NVNTVkc1dsSE9vVjFudXdhdzBrN0t2VlRYckhlWHlSWDhJMGxuK3U5Ymk0MjBGbkNTMlBOY3FaVWpia1Q3Z0RWVW9tL1I4QW5DRWhXWktnV1ZjR1lQM2k4aGF2cVdvWUNnNEQ1M2hmeThlOEFJWTZIUlAxWU84RnpIaWhDWWswV2pjQ3M4Yk1ydXBIcHJIbk9IMkV1NTBsSW5XMkZBZmhWR0RxTEM0VC9taytIblhiRHBuVFdwNnJ4cjV3VjJ4cUVNM3lPbjJSRThodHRMYXhYYnN3WGdKWE4wNnZFTmRqd1RtYmRJbHlubXN6VE9WbjRNZWg5WVd4RVFDQWZqK3R3OHhUQnJMSVFPRUNtOE55MXZER3AzNWlET2orQ1UvcjdURHQ2VjhiaXViMFNaSEh2Wng4di9OTzdkc1pTdUNuMTIzKzk4bTVkeC9rN1VXTHVTblQ3elZKQit6Q2NXUmUxY3ppaTI0dWs2TGZWUEUzSFBKTUpIbzdJTE9QWVFUbXhyRjBLVldVdXJKc2NVMGo4NWpxVFFORXltZllzOCtoeUhyalpsRFpkdzJvOWJrSTVYSy9rbVVTQWVhc2JvVGY1K2hpZ0JsTDZkL1dhMVlFaFpQSDgxcHlTeTBXU2dWYmRIK21odHV1eXR4RDFTUzNHUFIxWWNlejQySkllWHhjbDVYdlZqRVRjWVE4aWlIS1dRQVkzeW5sSTBWZXNGYW40Yis3Y2NsOWMrTFZqTzVRNEhTeEZkVGcwNlZPQTlFaXIwYjdySm51RTRQN3lwOVBLTG9hOHpPREI4NFpaQmlRdjVBNTBvVGtCdHVyQnk0NTR1OGFHaW0yYm9Dckl2RkhyZmpoMEJ1RmNYbE4zNEhkT0FCVVl4SUZtc1g1OUg0dVJva2FodFl5VWtmc05GYUFqZnlZQnd6Q3BXSGd1R0JqVE1wYU1uWWtHNkRxRWRFcithaC9WREg1TzdhQjczSmxtd2pncWhQNmRudVluL0wwdzcrZ3BwTzRDQkhSczFaSXoxNU5qYmxzZlE5cTF6cmlZVFBwdm5EM1BCTUExWFY5czlmTmRFMTZUSEdTSzJUQ01Va05EcWh2Wm1tVzFpTzI1c0k2RXlMTWM0SmsrQkw1RzhZSWZVWDhTTDZBelVUMU95dmR4TyIsImlhdCI6MTY1NzYzMDg5MSwiZXhwIjoxNjU4ODQwNDkxfQ.tZrgR-ddiPR84i9At68Tabnp9kAzIa-gcYkIhsMlWYg; _ym_visorc=w; v=1657632306; redirectMav=1; sx=H4sIAAAAAAAC%2F5zSSZKjMBCF4buwZqFUaqzbaDbIILDM2MHdO7xwhb2tC3zxK%2FX%2BNURjBGuMEUYRi4qiF4RSo5VHGj02P%2F%2BatflpqCv7Rud0pOKO6bkfe0eyXHZJce66R9M2ofkBwaUErQm92oYRVIyBJFFHzrWgVlJJNBpExazTb1meNub5uZBY%2FHLrPLuf9SScx%2BkwroufsqSor7bhSnuUxgMlStEIXgOA5DTyGK00v839nnOkZ9%2F7OUQR7byYwIvEuz36s5%2B%2BmpViL7mMpsiAbku1pJyLYmVLxdU3OUTZWxjqBDYBS924Au%2BB66GyPajlKxbUK1YIIZyXImqhuWBCB2kDai85cU763zOMEJd%2BPhPxGrbx6LW%2F96fGdch6evBPmRKkcLWN8hoCtZSisFw6GnUMTEUFjhBg6N7yrVvXM7HFbwDMgA1kC7V7kPG8P4d7%2FjqDlOpqGz2w%2BbGOa5gqcTnnXFUptar0Jm1az20LPAygt64cI0mYhIbHgz9V2D5JxilebWPzMekZl7kqV3LOLmdSSmXsd2DLMrqbNIcY1zR2w3SrZx86DtIMa%2Bw%2BSUXxNTBXXFBPJfzrjypLr8iSS61%2FqUQQV9t4wYV10hkuhdPBkBCUCcIwKrxRQr3lO2oyb%2F6IO6uVHhn9WAPdp5MwPt%2FO7%2FcjudomOhHQMWMYtzoEBOKVi5xQzjil4P%2FSDAyv638AAAD%2F%2F4pO1YvDAwAA; _mlocation=637640; _mlocation_mode=laas; _ga=GA1.2.418341391.1625657273; tmr_detect=0%7C1657633935657; tmr_reqNum=203; _ga_9E363E7BES=GS1.1.1657627701.64.1.1657633957.19' \
+
 search = 'склад'
 categoryId = 42
 locationId = 637640
 sort = 'priceDesc'
 withImagesOnly = 'false'
 limit_page = 50
+
 s = requests.Session()
-# https://m.avito.ru/api/1/rmp/search?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&params[536]=5546&params[110799]=472645&query=%D1%81%D0%BA%D0%BB%D0%B0%D0%B4&categoryId=42&locationId=637640&searchRadius=0
+
 headers = { 'authority': 'm.avito.ru',
             'pragma': 'no-cache',
             'cache-control': 'no-cache',
@@ -25,13 +28,12 @@ headers = { 'authority': 'm.avito.ru',
             'sec-fetch-user': '?1',
             'sec-fetch-dest': 'document',
             'accept-language': 'ru-RU,ru;q=0.9',}
-
-if cookie:                                    
+if cookie:                                      # Добавим куки, если есть внешние куки
     headers['cookie'] = cookie
-s.headers.update(headers)                      
-s.get('https://m.avito.ru/')                   
-url_api_9 = 'https://m.avito.ru/api/11/items'
-
+s.headers.update(headers)                       # Сохраняем заголовки в сессию
+s.get('https://m.avito.ru/')                    # Делаем запрос на мобильную версию.
+url_api_9 = 'https://m.avito.ru/api/11/items'    # Урл первого API, позволяет получить id и url объявлений по заданным фильтрам
+                                                # Тут уже видно цену и название объявлений
 params = {
     'categoryId': categoryId,
     'params[536]': 5546,
@@ -43,13 +45,14 @@ params = {
     'display': 'list',
     'limit': limit_page,
     'query': search,
-    'key': key,
 }
+
+params['key'] =  key
 
 
 def writeInLog(message, location):
     f = open('log.txt', mode = 'a')
-    f.write(f'Loc: {location}: {message}')
+    f.write(f'Loc: {location}: {message}\n')
 
 def getIDS(filename = 'ids.ini'):
     ids = []
@@ -64,6 +67,8 @@ def getIDS(filename = 'ids.ini'):
         res = s.get(url_api_9, params=params)
         try:
             res = res.json()
+            f = open('test.json', mode = 'w')
+            json.dump(res, f)
 
         except json.decoder.JSONDecodeError:
             cicle_stop = False
@@ -100,27 +105,26 @@ def getInfo(id):
     }
     info_js = s.get(url_info, params=params).json()
     if not 'error' in info_js:
+        f = open('test.json', mode = 'w')
+        json.dump(info_js, f)
         return info_js
-        # f = open('test.json', mode = 'w')
-        # json.dump(info_js, f)
     else:
         print(info_js)
         
-
-# additionalSeller.parameters
 
 def ParseInfo(info, title_csv):
     try:
         info_parse = []
         for i in range(len(title_csv)):
             info_parse.append('')
-            
-        info_parse[0] = info['title']
-        info_parse[1]  = info['address']
-        info_parse[2]  = info['sharing']['url']
-        info_parse[3] = info['price']['value']
-        info_parse[4] = info['coords']['lat']
-        info_parse[5] = info['coords']['lng']
+        info_parse[0] = info['firebaseParams']['itemID']
+        info_parse[1] = info['title']
+        info_parse[2]  = info['address']
+        info_parse[3]  = info['sharing']['url']
+        info_parse[4] = info['price']['value']
+        info_parse[5] = info['price']['metric']
+        info_parse[6] = info['coords']['lat']
+        info_parse[7] = info['coords']['lng']
         
         additionalSeller = info['additionalSeller']['parameters']
         for parameter in additionalSeller:
@@ -140,19 +144,42 @@ def ParseInfo(info, title_csv):
                 print(f'{title_p} не учтен!')
                 writeInLog(f'{title_p} не учтен!', 'parseMessage')
                     
-        info_parse[25] = info['description']
+        info_parse[27] = info['description']
+
         images_str = ''
-        for image in info['images']:
-            images_str += image['1280x960'] + '; '
-        info_parse[26] = images_str
+
+        try:
+            for image in info['images']:
+                images_str += image['1280x960'] + '; '
+        except Exception:
+            pass
+
+        info_parse[28] = images_str
          
-        info_parse[27] = info['time']
-        info_parse[28] = info['seller']['postfix']
-        info_parse[29] = info['seller']['name']
-        info_parse[30] = info['seller']['manager']
+        info_parse[29] = info['time']
+
+        try:
+            info_parse[30] = info['seller']['postfix']
+        except Exception:
+            pass
+        try:
+            info_parse[31] = info['seller']['name']
+        except Exception:
+            pass
+        try:
+            info_parse[32] = info['seller']['manager']
+        except Exception:
+            pass
 
         user_hash = info['seller']['userHash']
-        info_parse[31] = f'https://www.avito.ru/user/{user_hash}/profile'
+        info_parse[33] = f'https://www.avito.ru/user/{user_hash}/profile'
+        try:
+            url_get_phone = 'https://m.avito.ru/api/1/items/' + str(info['firebaseParams']['itemID']) + '/phone'
+            phone = s.get(url_get_phone, params=params).json()
+            info_parse[34] = requests.utils.unquote(phone['result']['action']['uri'].split('number=')[1])
+        except Exception as e:
+            print(e)
+            writeInLog(f'Error phone_number', 'parseMessage')
         return info_parse
     
     except Exception as e:
@@ -166,38 +193,41 @@ def SaveInfo(info, file_name = 'data.csv'):
         writer = csv.writer(csvFile)
         writer.writerow(info)
 
-
 def main():
-    print(s.get('https://m.avito.ru/api/18/items/2338321304?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&action=view&context=H4sIAAAAAAAA_0q0MrSqLrYytFKqULIutjI2tFIytCiwLEs2qSyzMK3Iqyw2yEwrSzc2NM03zs3Ir0hOU7KuBQQAAP__5cJxMDUAAAA').text)
-    # title_csv = ['Название','Адрес','URL',
-    #          'Цена','Координаты lat','Координаты lng',
-    #          'Тип здания', 'Класс здания', 'Общая площадь', 
-    #          'Этаж', 'Отделка', 'Залог', 
-    #          'Комиссия, %', 'Категория', 'Парковка', 
-    #          'Тип парковки', 'Отдельный вход','Общая площадь',
-    #          'Аренда части площади','Высота потолков, м','Включено в стоимость',
-    #          'Отопление','Отделка','Вход',
-    #          'Отдельный вход', 'Описание','Изображения', 
-    #          'Дата опубликования', 'Тип продавца','Название компании',
-    #          'Имя продавца','URL продавца']
+    title_csv = ['ID', 'Название','Адрес','URL',
+             'Цена','ЕИ цены','Координаты lat','Координаты lng',
+             'Тип здания', 'Класс здания', 'Общая площадь', 
+             'Этаж', 'Отделка', 'Залог', 
+             'Комиссия, %', 'Категория', 'Парковка', 
+             'Тип парковки', 'Отдельный вход',
+             'Аренда части площади','Высота потолков, м','Включено в стоимость', 'Несколько этажей',
+             'Отопление','Отделка','Вход',
+             'Отдельный вход', 'Описание','Изображения', 
+             'Дата опубликования', 'Тип продавца','Название компании',
+             'Имя продавца','URL продавца', 'Номер телефона']
     
-    # file_ids = getIDS('ids.ino')
+    file_ids = getIDS('ids.ini')
     # file_ids = 'ids.ini'
     # f = open(file_ids, mode = 'r')
     # ids = f.readlines()
-    # SaveInfo(title_csv)
+    # # SaveInfo(title_csv)
     # for i in range(len(ids)):
     #     info = getInfo(ids[i].strip())
-    #     parse_info = ParseInfo(info, title_csv)
+    #     parse_info = ParseInfo(info=info, title_csv=title_csv)
     #     if parse_info:
     #         SaveInfo(parse_info)
+    #         print('Добавлено ' + str(i+1) + ' объявлений')
     #     else:
     #         print(f'Ошибка на {i} объявлении, id: {ids[i]}')
     #         writeInLog(f'Ошибка на {i} объявлении, id: {ids[i]}', 'main')
     #         input('Нажмите Enter чтобы продолжить...')
+    #     time.sleep(5)
     # f = open('test.json', mode = 'r')
     # info = json.load(f)
     # ParseInfo(info)
+
+    
+
 
 if __name__ == '__main__':
     main()
@@ -205,9 +235,6 @@ if __name__ == '__main__':
 
 
 
-#         url_get_phone = 'https://m.avito.ru/api/1/items/' + ad_id + '/phone'    # URL для получения телефона
-#         phone = s.get(url_get_phone, params=params).json()                      # Сам запрос
-#         if phone['status'] == 'ok': phone_number = requests.utils.unquote(phone['result']['action']['uri'].split('number=')[1]) # Прверка на наличие телефона, такой странный синтсксис, чтоб уместиться в 100 сторочек кода)))
-#         else: phone_number = phone['result']['message']
-
+        
+# https://m.avito.ru/api/11/items?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&query=склад&categoryId=42&locationId=637640&params[536]=5546&params[554]=5727&context=H4sIAAAAAAAA_wFCAL3_YToxOntzOjU6Inhfc2d0IjtzOjQwOiJiZmJjNzJkYzY4YmZmNGYyYjZlM2E3NTQ3M2M1NTUwYTk0YTE2MDE1Ijt95w3TfEIAAAA&page=3&lastStamp=1657706700&display=list&limit=25&pageId=H4sIAAAAAAAA_0q0MrSqLrYyNLRSKskvScyJT8svzUtRss60MjM3N7GuBQQAAP__vqFwuCAAAAA&presentationType=serp
 
