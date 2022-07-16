091422cd8d003f94448414c8cbe6b05dfe0efe2e
PavelG1307 <julu13@yandex.ru>
2022-07-16 21:14:56 +0300

    vv

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import psycopg2
+import uuid
 
 class DBController:
     def __init__(self, dbname, user, password, host):
@@ -15,22 +16,37 @@ class DBController:
     def getStructures(self):
         self.cursor.execute(f'SELECT name, uuid, parents FROM {self.structures_table} ')
         self.structures = {}
-        resp = self.cursor.fetchall()
-        for i in range(len(resp)):
-            self.structures[resp[i][0]] = resp[i][1]
+        self.uuid_i = {}
+        self.resp = self.cursor.fetchall()
+        for i in range(len(self.resp)):
+            self.structures[self.resp[i][0]] = self.resp[i][1]
+            self.uuid_i[self.resp[i][1]] = self.resp[i][0]
         
     def close(self):
         self.cursor.close()
         self.onn.close()
 
+    def InsertIntoParrent(self, value, parent):
+        for item in self.resp:
+            if item[0] == value:
+                name_parent = self.uuid_i[item[2][1:-1].split(',')[0]]
+                if name_parent == parent:
+                    self.parents.append(item[1])
+                    break
+                
+        
     def saveItems(self, item):
         structures = self.structures
-        parents = []
+        self.parents = []
         if item['Тип'] == 'container':
-            parents.append(structures['Контейнер'])
+            self.parents.append(structures['Контейнер'])
         elif item['Тип'] == 'warehouse': 
-            parents.append(structures['Склад'])
-        # if 
+            self.parents.append(structures['Склад'])
+        elif item['Тип'] == 'space':
+            self.parents.append(structures['Площадка'])
+        elif item['Тип'] == 'box':
+            self.parents.append(structures['Площадка'])
+
         avito_id = item['ID']
         name = item['Название']
         address = item['Адрес']
@@ -47,7 +63,43 @@ class DBController:
         elif item['ЕИ цены'] == '₽ в год за м²':
             price = price * capacity
             interval = 'years'
-        coords = {item["Координаты lat"], item["Координаты lng"]}
-        print(parents)
+            
+        
+        # url_seller = item['URL продавца']
+        # name_seller = item['Название компании']
+        # type_seller = item['Тип продавца']
+        # time_load = item['Дата опубликования']
+        
+            # 'Аренда части площади'
+            # 'Номер телефона'
+            # 'Имя продавца'
+            
+        titles = ['Тип здания', 'Классы склада', 'Этаж', 'Отделка', 'Залог','Парковка','Тип парковки','Отдельный вход','Категория', 'Несколько этажей',
+            'Отопление','Вход', 'Количество парковочных мест','Описание','Изображения', 'Комиссия, %','Включено в стоимость', 'Аренда части площади']
+        
+        for title in titles:
+            try:
+                value = item[title]
+                self.InsertIntoParrent(value, title)
+            except Exception:
+                pass
+        if item['Высота потолков, м']:
+            h = float(item['Высота потолков, м'])
+            if h <= 1:
+                self.InsertIntoParrent('1', 'Высота потолка, м')
+            elif h <= 2:
+                self.InsertIntoParrent('2', 'Высота потолка, м')
+            elif h <= 3:
+                self.InsertIntoParrent('3', 'Высота потолка, м')
+            elif h <= 4:
+                self.InsertIntoParrent('4', 'Высота потолка, м')
+            elif h <= 5:
+                self.InsertIntoParrent('5', 'Высота потолка, м')
+            elif h > 5:
+                self.InsertIntoParrent('Более 5 метров', 'Высота потолка, м')
+        # value = item[titles[1]]
+        # self.InsertIntoParrent(value, titles[1])
+        # coords = {item["Координаты lat"], item["Координаты lng"]}
+        print(self.parents)
         # print(self.executeAll('structures3'))
         pass
