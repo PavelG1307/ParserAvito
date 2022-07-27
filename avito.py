d0c349919b73160104cfbc8361944f3f4145d664
Павел Горшков <100727722+PavelG1307@users.noreply.github.com>
2022-07-27 16:29:14 +0300

    Update avito.py

--- avito.py
+++ avito.py
@@ -1,6 +1,6 @@
-import requests
+from session import Session
 import json
-import time
+from helpers import add_speciality_symbol
 
 class Avito():
 
@@ -16,22 +16,7 @@ class Avito():
 
     def raiseSession(self):
         try:
-            s = requests.Session()
-            headers = {
-                    'authority': 'm.avito.ru',
-                    'pragma': 'no-cache',
-                    'cache-control': 'no-cache',
-                    'upgrade-insecure-requests': '1',
-                    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Mobile Safari/537.36',
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                    'sec-fetch-site': 'none',
-                    'sec-fetch-mode': 'navigate',
-                    'sec-fetch-user': '?1',
-                    'sec-fetch-dest': 'document',
-                    'accept-language': 'ru-RU,ru;q=0.9',
-                    'cookie': self.cookie,
-                    }
-            s.headers.update(headers)
+            s = Session()
             self.session = s
             return True
         except Exception as e:
@@ -45,52 +30,61 @@ class Avito():
 
 
     def getIDS(self, filename = './assets/ids/ids.ini'):
-        ids = []
-        cicle_stop = True       
-        cikle = 0               
+        ids = []                  
         items = []
         url_api_9 = 'https://m.avito.ru/api/11/items'
+        f = open(filename, mode = 'w')     
 
-        f = open(filename, mode = 'w')             
-        while cicle_stop:
-            cikle += 1
-            self.params['page'] = cikle
-            page = self.params['page']
-            print(f'Страница: {page}')
-            res = self.session.get(url_api_9, params=self.params)
-            try:
-                res = res.json()
 
-            except json.decoder.JSONDecodeError:
-                cicle_stop = False
-                break
+        price_cat = [None, 5000, 10000, 20000, 40000, 70000, 130000, 300000, 900000, None]
+        for i in range(len(price_cat) - 1):
+            cicle_stop = True 
+            if price_cat[i]:
+                self.params['priceMin'] = price_cat[i]
+                print(f'Цена от {price_cat[i]}')
+            else: 
+                if 'priceMin' in self.params.keys():
+                    del self.params['priceMin']
+            if price_cat[i + 1]:
+                self.params['priceMax'] = price_cat[i + 1]
+                print(f"Цена до {self.params['priceMax']}")
+            else: 
+                if 'priceMax' in self.params.keys():
+                    del self.params['priceMax']
+            cikle = 0   
 
-            if res['status'] != 'ok':
+            while cicle_stop:
+                cikle += 1
+                self.params['page'] = cikle
+                page = self.params['page']
+                print(f'Страница: {page}')
+                res = self.session.get(url_api_9, params=self.params)
+                if 'status' in res.keys():
+                    if res['status'] != 'ok':
+                            print(res)
+
+                    if res['status'] == 'ok':
+                        items_page = int(len(res['result']['items']))
+
+                        if items_page > self.limit_page:
+                            items_page = items_page - 1
+
+                        for item in res['result']['items']:
+                            if item['type'] == 'item':
+                                items.append(item)
+                            elif item['type'] == 'groupTitle':
+                                cicle_stop = False
+                        if items_page < self.limit_page:
+                            cicle_stop = False
+                    for item in items:
+                        if item['type'] == 'item' or item['type'] == 'xlItem':
+                            ad_id = str(item['value']['id'])
+                            if not ad_id in ids:
+                                ids.append(ad_id)
+                                f.write(str(ad_id) + '\n')
+                else:
                     print(res)
-                    raise('error in getIds')
-
-            if res['status'] == 'ok':
-                items_page = int(len(res['result']['items']))
-
-                if items_page > self.limit_page:
-                    items_page = items_page - 1
-
-                for item in res['result']['items']:
-                    print(item['type'])
-                    if item['type'] == 'item':
-                        items.append(item)
-                    elif item['type'] == 'groupTitle':
-                        cicle_stop = False
-                        break
-                if items_page < self.limit_page:
-                    cicle_stop = False
-            for item in items:
-                if item['type'] == 'item' or item['type'] == 'xlItem':
-                    ad_id = str(item['value']['id'])
-                    if not ad_id in ids:
-                        ids.append(ad_id)
-                        f.write(str(ad_id) + '\n')
-        print(f'Всего объявлений: {len(ids)}')
+            print(f'Всего объявлений: {len(ids)}')
         self.fileIds = filename
 
 
@@ -117,35 +111,29 @@ class Avito():
             print(f'Авито объявления: {page}')
             res = self.session.get(url, params=params)
 
-            try:
-                res = res.json()
+            if 'status' in res: 
+                if res['status'] != 'ok':
+                        print(res)
+                        # raise('api error')
 
-            except json.decoder.JSONDecodeError:
-                cicle_stop = False
-                break
+                if res['status'] == 'ok':
+                    items_page = int(len(res['result']['items']))
 
-            if res['status'] != 'ok':
-                    print(res)
-                    raise('api error')
+                    if items_page > self.limit_page:
+                        items_page = items_page - 1
 
-            if res['status'] == 'ok':
-                items_page = int(len(res['result']['items']))
-
-                if items_page > self.limit_page:
-                    items_page = items_page - 1
-
-                for item in res['result']['items']:
+                    for item in res['result']['items']:
+                        if item['type'] == 'item':
+                            if item['value']['category']['id'] == category_id:
+                                items.append(item)
+                    if items_page < self.limit_page:
+                        cicle_stop = False
+                for item in items:
                     if item['type'] == 'item':
-                        if item['value']['category']['id'] == category_id:
-                            items.append(item)
-                if items_page < self.limit_page:
-                    cicle_stop = False
-            for item in items:
-                if item['type'] == 'item':
-                    ad_id = str(item['value']['id'])
-                    if not ad_id in ids:
-                        ids.append(ad_id)
-                        f.write(str(ad_id) + '\n')
+                        ad_id = str(item['value']['id'])
+                        if not ad_id in ids:
+                            ids.append(ad_id)
+                            f.write(str(ad_id) + '\n')
 
         print(f'Всего объявлений: {len(ids)}')
 
@@ -167,14 +155,13 @@ class Avito():
     def getInfo(self, id):
         url_info = f'https://m.avito.ru/api/18/items/{id}'
         self.params = {'key': self.key}
-        info_js = self.session.get(url_info, params=self.params).json()
+        info_js = self.session.get(url_info, params=self.params)
         if not 'error' in info_js:
             f = open('./temp/log_req.json', mode = 'w')
             json.dump(info_js, f)
             return info_js
         else:
             print(info_js)
-            raise('error')
 
 
     def insertToResp(self, data, title):
@@ -195,11 +182,14 @@ class Avito():
         try:
             if 'контейнер' in info['description'].lower():
                 self.insertToResp('container', 'Тип')
-                area = float(info['firebaseParams']['area'].split(' ')[0])
-                if area <= 15 and area >= 10:
-                    self.insertToResp('20', 'Объем контейнера')
-                elif area <= 21 and area >= 20:
-                    self.insertToResp('20', 'Объем контейнера')
+                try:
+                    area = float(info['firebaseParams']['area'].split(' ')[0])
+                    if area <= 15 and area >= 10:
+                        self.insertToResp('20', 'Объем контейнера')
+                    elif area <= 21 and area >= 20:
+                        self.insertToResp('40', 'Объем контейнера')
+                except Exception:
+                    pass
             elif 'площадк' in info['description'].lower():
                 self.insertToResp('space', 'Тип')
             elif 'бокс' in info['description'].lower():
@@ -210,8 +200,8 @@ class Avito():
                 self.insertToResp('', 'Тип')
 
             self.insertToResp(info['firebaseParams']['itemID'], 'ID')
-            self.insertToResp(info['title'], 'Название')
-            self.insertToResp(info['address'], 'Адрес')
+            self.insertToResp(add_speciality_symbol(info['title']), 'Название')
+            self.insertToResp(add_speciality_symbol(info['address']), 'Адрес')
             self.insertToResp(info['sharing']['url'], 'URL')
             self.insertToResp(info['price']['value'], 'Цена')
             self.insertToResp(info['price']['metric'], 'ЕИ цены')
@@ -225,8 +215,9 @@ class Avito():
             parameters = info['parameters']['flat']
             for parameter in parameters:
                 self.insertToResp(parameter['description'], parameter['title'])
-                
-            self.insertToResp(info['description'], 'Описание')
+            description = info['description']
+            description = add_speciality_symbol(description)
+            self.insertToResp(description, 'Описание')
             images_str = ''
             image_json = {}
 
@@ -248,24 +239,16 @@ class Avito():
             except Exception:
                 pass
             try:
-                self.insertToResp(info['seller']['name'], 'Название компании')
+                self.insertToResp(add_speciality_symbol(info['seller']['name']), 'Название компании')
             except Exception:
                 pass
             try:
-                self.insertToResp(info['seller']['manager'], 'Имя продавца')
+                self.insertToResp(add_speciality_symbol(info['seller']['manager']), 'Имя продавца')
             except Exception:
                 pass
 
             user_hash = info['seller']['userHash']
-            self.insertToResp(f'https://www.avito.ru/user/{user_hash}/profile', 'URL продавца')
-            try: 
-                url_get_phone = 'https://m.avito.ru/api/1/items/' + str(info['firebaseParams']['itemID']) + '/phone'
-                phone = self.session.get(url_get_phone, params=self.params).json()
-                phone_number = requests.utils.unquote(phone['result']['action']['uri'].split('number=')[1])
-                self.insertToResp(phone_number, 'Номер телефона')
-            except Exception as e:
-                print(e)
-                self.writeInLog(f'Error phone_number', 'parseMessage')
+            self.insertToResp(user_hash, 'Hash продавца')
             return True
         except Exception as e:
             self.writeInLog(f'Error {e}', 'parseMessage')
@@ -273,13 +256,13 @@ class Avito():
             return None
 
 
-    def parse(self, categoryId, locationId, columns, search = 'Склад', user_id_hash = None, save_title=True, only_ids=False, only_info=False, fileIds='./assets/ids/ids.ini', sort = 'priceDesc', withImagesOnly = 'false', priceMin=None, priceMax=None):
+    def parse(self, categoryId, locationId, columns, callback_save, search = 'Склад', user_id_hash = None, save_title=True, only_ids=False, only_info=False, fileIds='./assets/ids/ids.ini', sort = 'priceDesc', withImagesOnly = 'false', priceMin=None, priceMax=None):
         print('Start parsing')
         self.raiseSession()
         self.search = search
         self.categoryId = categoryId
         self.locationId = locationId
-        self.ads = []
+        self.ads = {}
         self.params = {
             'categoryId': categoryId,
             'params[536]': 5546,
@@ -308,7 +291,8 @@ class Avito():
                 self.get_ids_from_user(user_id_hash)
         if (only_ids):
             return self.file
-        
+        if (only_info):
+            self.fileIds = './assets/ids/ids.ini'
         ids = self.readIds(filename = self.fileIds)
         for i in range(len(ids)):
             info = self.getInfo(ids[i].strip())
@@ -316,15 +300,20 @@ class Avito():
             for j in range(len(columns)):
                 self.response.append('')
             self.json_resp = {}
-            parse_info = self.ParseInfo(info=info)
+            try:
+                parse_info = self.ParseInfo(info=info)
+            except Exception as e:
+                print(e)
+                parse_info = None
 
             if parse_info:
-                self.ads.append(self.json_resp)
+                uuid, data = callback_save(self.json_resp, 'structures4')
+                self.ads[uuid] = data
                 print('Полученно ' + str(i+1) + ' объявлений')
             else:
                 print(f'Ошибка на {i} объявлении, id: {ids[i]}')
                 self.writeInLog(f'Ошибка на {i} объявлении, id: {ids[i]}', 'main')
-                raise 'Error in ParseInfo'
-            time.sleep(self.timeout)
+
+ 
         return self.ads
 
